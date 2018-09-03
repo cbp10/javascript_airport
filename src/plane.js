38cc96bcf79cf26702bdeb2a49426df7a865428b
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-03 17:37:17 +0100

    gTest for Airport responding to land

---
+++ src/plane.js
@@ -0,0 +1,13 @@
+function Plane() {
+}
+
+
+
+
+function Song() {
+}
+
+Song.prototype.persistFavoriteStatus = function(value) {
+  // something complicated
+  throw new Error("not yet implemented");
+};
